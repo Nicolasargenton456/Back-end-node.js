f303559d0e8612103ec7214ed002d9735e74c3ca
Nicolasargenton456 <ds.46276553808@gmail.com>
2024-09-20 09:01:56 -0300

    Atualizado

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-import { getAllProducts } from "../services/productService.js";
+import { createProducts , getAllProducts, getProductById } from "../services/productService.js";
 
 export const getProducts = async (req, res) => {
     try {
@@ -8,6 +8,30 @@ export const getProducts = async (req, res) => {
     catch (error) {
         res.status(500).json({
             error: "Erro ao buscar produtos",
-        })
+        });
     }
-}
+};
+
+export const createProductController = async(req,res) => {
+    try{
+        const newProduct = await createProducts(req.body);
+        res.status(201).json(newProduct);
+    }
+    catch(error) {
+        res.status(500).json({error: "Erro ao criar produto"});
+    }
+};
+
+export const getProduct = async(req, res)=>{
+    try { 
+        const product = await getProductById(req.params.id)
+        if(product){
+            res.json(product);
+        }else{
+            res.status(500).json({error: "Produto não encontrado"});
+        }
+    }
+    catch (error){
+        res.status(500).json({error: "Erro ao buscar produto"});
+    }
+};
